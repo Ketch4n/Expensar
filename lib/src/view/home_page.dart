a37df32129967dbe3d11eeae0e12304c48952cfa
Ketch4n <mangao.christian.04@gmail.com>
2026-01-20 16:53:08 +0800

    feat: add HTTP package and implement ping functionality in homepage

--- lib/src/view/home_page.dart
+++ lib/src/view/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:expensar/src/utils/app_bar_utils.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -10,13 +11,44 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _pingGoogle() async {
+    try {
+      final response = await http.get(Uri.parse('https://www.google.com'));
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Ping successful!')));
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Ping failed!')));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Error: $e')));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'Expensar'),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [const Text('HOMEPAGE - TEST PATCH v0.2.1+1')],
+          children: [
+            const Text('HOMEPAGE - TEST PATCH v0.2.1+1'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pingGoogle,
+              child: const Text('Ping Google'),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
